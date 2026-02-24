353a6f688de4734ea337c2e1e31d5835106620b9
Salman-S-Rahman <salmanmern@gmail.com>
2026-02-24 11:09:26 +0100

    filter functionality added

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,37 @@ let allJobCards = document.getElementById("all_job_cards");
 let totalCount = document.getElementById("total_count");
 let interviewCount = document.getElementById("interview_count");
 let rejectedCount = document.getElementById("rejected_count");
+let allFilterBtn = document.getElementById("all_filter_btn");
+let interviewFilterBtn = document.getElementById("interview_filter_btn");
+let rejectedFilterBtn = document.getElementById("rejected_filter_btn");
 
-console.log(allJobCards.children.length)
+let mainPart = document.querySelector("main");
 
-function 
+let currentStatus = "";
+let interviewList = [];
+let rejectedList = [];
+
+
+function calculateCount (){
+    totalCount.innerText = allJobCards.children.length
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculateCount()
+
+function toggleStyle(id){
+    currentStatus=id;
+
+    allFilterBtn.classList.add ("bg-gray-200", "text-gray-700");
+    interviewFilterBtn.classList.add ("bg-gray-200", "text-gray-700");
+    rejectedFilterBtn.classList.add ("bg-gray-200", "text-gray-700");
+
+    allFilterBtn.classList.remove("bg-blue-600", "text-white");
+    interviewFilterBtn.classList.remove("bg-blue-600", "text-white");
+    rejectedFilterBtn.classList.remove("bg-blue-600", "text-white");
+
+    const selected = document.getElementById(id)
+
+    selected.classList.remove("bg-gray-200", "text-gray-700");
+    selected.classList.add("bg-blue-600", "text-white");
+}
