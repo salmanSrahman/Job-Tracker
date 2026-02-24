d7adb78561a7920c5a0635ad57ba1fde12ccd550
Salman-S-Rahman <salmanmern@gmail.com>
2026-02-24 10:39:13 +0100

    job tracker app using html, tailwind css & js. Design part end

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let allJobCards = document.getElementById("all_job_cards");
+let totalCount = document.getElementById("total_count");
+let interviewCount = document.getElementById("interview_count");
+let rejectedCount = document.getElementById("rejected_count");
+
+console.log(allJobCards.children.length)
+
+function 
